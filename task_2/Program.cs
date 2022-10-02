c1428aed2e6cb8f1502f9162536fbe695941379b
Elena <pyatochkaa@mail.ru>
2022-10-02 22:54:54 +0300

    task_2

---
+++ task_2/Program.cs
@@ -0,0 +1,10 @@
+﻿
+Console.WriteLine("Enter the first number.");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter the second number.");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+if(firstNumber > secondNumber) {
+    Console.WriteLine("Pure " + firstNumber + " is greater than the number " + secondNumber);
+} else Console.WriteLine("Pure " + secondNumber + " is greater than the number " + firstNumber);
