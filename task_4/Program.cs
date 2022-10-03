e311815b9dd8c3e2732cf87da41729e693a66e7b
Elena <pyatochkaa@mail.ru>
2022-10-03 11:05:40 +0300

    test

---
+++ task_4/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Enter the first number.");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter the second number.");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter the third number.");
+int thirdNumber = Convert.ToInt32(Console.ReadLine());
+
+if (firstNumber > secondNumber && firstNumber > thirdNumber)
+{
+    Console.WriteLine("maximum number " + firstNumber);
+}
+if (secondNumber > firstNumber && secondNumber > thirdNumber)
+{
+    Console.WriteLine("maximum number " + secondNumber);
+}
+if (thirdNumber > firstNumber && thirdNumber > secondNumber)
+{
+    Console.WriteLine("maximum number " + thirdNumber);
+}
