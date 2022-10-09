01bdb1ef49accfd91f81d0e07f00e8d8b36cb98e
Elena <pyatochkaa@mail.ru>
2022-10-09 19:52:09 +0300

    Homework: tasks 10, 13, 15

---
+++ task_10/Program.cs
@@ -0,0 +1,9 @@
+﻿
+Console.WriteLine("Введите трехзначное число.");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+int number = num / 10;
+int secondNumber = number % 10;
+
+Console.WriteLine(" Не знаю зачам вам это надо знать, но второе число " + secondNumber);
