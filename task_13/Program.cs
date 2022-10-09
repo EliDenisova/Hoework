1b2f19bed12777c9f5caff721c5f85b20112e5a3
Elena <pyatochkaa@mail.ru>
2022-10-09 20:07:43 +0300

    пытаюсь добавить дз

---
+++ task_13/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число");
+
+int number = Convert.ToInt32(Console.ReadLine());
+string numberText = Convert.ToString(number);
+
+if (numberText.Length < 2)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    Console.WriteLine("Третья цифра " + numberText[2]);
+}
---
+++ task_15/Program.cs
@@ -0,0 +1,25 @@
+﻿
+Console.WriteLine("Введите номер деня недели");
+
+int weekDay = 0;
+while (true)
+{
+    weekDay = Convert.ToInt32(Console.ReadLine());
+
+    if (weekDay > 0 && weekDay <= 7)
+    {
+        break;
+    }
+    
+    {
+        Console.WriteLine("на нашей планете нет такиих дней недели.");
+        Console.WriteLine("");
+        Console.WriteLine("Повторите ввод");
+    }
+}
+
+if (weekDay == 6 || weekDay == 7)
+{
+    Console.WriteLine("Надеваем мягкую пижамку и наливаем какао, ведь сегодня выходной!");
+}
+else Console.WriteLine("Снимаем пижамку, пьем кофе и идем на работу. Сегодня будний день.");
