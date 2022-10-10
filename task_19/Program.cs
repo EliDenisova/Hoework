f97fd7b568e9f6125b275d243a4eb1bf642cba2a
Elena <pyatochkaa@mail.ru>
2022-10-10 22:20:47 +0300

    исправила задачу 19

--- task_19/Program.cs
+++ task_19/Program.cs
@@ -2,14 +2,17 @@
 
 string number = Convert.ToString(Console.ReadLine());
 
-for (int i = 0; i < number.Length/2; i++)
+bool palindrom = true;
+for (int i = 0; i < number.Length / 2; i++)
 {
     if (number[i] != number[number.Length - 1 - i])
     {
-        Console.WriteLine("Это не полиндром");
-    }
-    else
-    {
-        Console.WriteLine("Это полиндром");
+        palindrom = false;
     }
 }
+
+if (palindrom)
+{
+    Console.WriteLine("Это палиндром");
+}
+else Console.WriteLine("Это не палиндром");
