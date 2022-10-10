b634a5b259215a75d81bc99c6566fcd67b42aa5b
Elena <pyatochkaa@mail.ru>
2022-10-10 22:07:28 +0300

    add tasks 19, 21, 23

---
+++ task_19/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число.");
+
+string number = Convert.ToString(Console.ReadLine());
+
+for (int i = 0; i < number.Length/2; i++)
+{
+    if (number[i] != number[number.Length - 1 - i])
+    {
+        Console.WriteLine("Это не полиндром");
+    }
+    else
+    {
+        Console.WriteLine("Это полиндром");
+    }
+}
