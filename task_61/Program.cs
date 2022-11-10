59ee8670ed653627c250258ed4259d11c8cee3ad
Elena <pyatochkaa@mail.ru>
2022-11-10 20:10:07 +0300

    add task 61

---
+++ task_61/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+int i, n, c;
+Console.WriteLine("Введите нужное количество строк треугольника Паскаля:");
+string s = Console.ReadLine();
+n = Convert.ToInt32(s);
+Console.WriteLine($"Ваше число {n}");
+
+Console.WriteLine($"Треугольник Паскаля ");
+pascalTriangle(n);
+
+    void pascalTriangle(int n)
+    {
+        for (int line = 1; line <= n; line++)
+        {
+            for (int j = 0; j <= (n - line); j++)
+                {
+                    Console.Write(" "); 
+                }
+            int c = 1;
+            for (int i = 1; i <= line; i++)
+            {
+                Console.Write(" ");
+                Console.Write(c);
+                c = c * (line - i) / i;
+            }
+            Console.WriteLine(" ");
+        }
+    }
+
+
