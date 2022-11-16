418eb2af3ab111a58671bfd135fe75a28245c6f0
Elena <pyatochkaa@mail.ru>
2022-11-16 21:15:31 +0300

    add task 66

---
+++ task_66/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число число для начала счета");
+int from = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число число для окончания счета");
+int to = Convert.ToInt32(Console.ReadLine());
+
+printNaturalNumbers(from, to, 0);
+
+
+void printNaturalNumbers(int numberStart, int numberStop, int sum) {
+
+    if (numberStart > numberStop) 
+    {
+        Console.WriteLine($"Сумма элементов {sum}"); 
+        return;
+    }
+    sum += (numberStart ++);
+    printNaturalNumbers(numberStart, numberStop, sum);
+}
