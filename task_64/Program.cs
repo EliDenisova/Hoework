dae4d4df441e35619f0556fc9e994ae01d46ddc6
Elena <pyatochkaa@mail.ru>
2022-11-16 21:09:38 +0300

    add task 64

---
+++ task_64/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+printNaturalNumbers(number);
+
+
+void printNaturalNumbers(int n) {
+
+    if(n == 1 ) {
+        Console.Write(n);
+    } else {
+        Console.Write(n);
+        printNaturalNumbers(n-1);
+    }
+}
+
