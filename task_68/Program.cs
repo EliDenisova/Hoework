cf59cd01763e68203470cc3027d1aff8f54294ff
Elena <pyatochkaa@mail.ru>
2022-11-16 21:11:23 +0300

    add task 68

---
+++ task_68/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите число число M");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число число N");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Значение функции {ackermannFunction(numberM, numberN)}");
+
+int ackermannFunction(int m, int n) {
+
+    if(m == 0) {
+        return n + 1;
+    }
+    if(m > 0 && n == 0) {
+        return ackermannFunction(m - 1, 1);
+    }
+    if(m > 0 && n > 0) {
+       return ackermannFunction(m - 1, ackermannFunction(m, n - 1));
+    }
+    return ackermannFunction(m, n);
+}
